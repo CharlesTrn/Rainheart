3cc7e79894de0ec29436af867dfb78e23cd7c42a
CharlesTrn <charles.tarnier@gmail.com>
2017-10-09 22:54:51 +0200

    Ajouter zone button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  index: number;
+  zones: Array<{number: number, icon: string}>;
   constructor(public navCtrl: NavController) {
+    this.zones = [];
+    this.index = 0;
+  }
 
+  addZone() {
+    this.index += 1;
+    this.zones.push({
+      number: this.index,
+      icon: ""
+    });
   }
 
 }
